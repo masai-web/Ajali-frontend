11620dbb3f2a143f31be48b36f8d9da93d406474
Leslie Kogi <lesliekogi@gmail.com>
2025-07-21 22:02:47 +0300

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import Admin from "../pages/Admin";
+import Incidents from "../pages/Incidents";
 
 function App() {
-return(
-  <h1>Ajali</h1>
-)
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/incidents" element={<Incidents />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
