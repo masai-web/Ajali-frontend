60d3232200493c5be64cebde5ae8cd407b6d61c0
Leslie Kogi <lesliekogi@gmail.com>
2025-07-29 21:49:07 +0300

    change vite config to port 5173

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server:{
-
-    https:{
-
-
-      key: './ajalifrontend-privateKey.key' ,
+  server: {
+    port: 5173, // 👈 add this line
+    https: {
+      key: './ajalifrontend-privateKey.key',
       cert: './ajalifrontend.crt',
     }
-
   },
   plugins: [react()],
   define: {
